b58b6c5ec85a782c462563f6501c30c1fea8a6d9
davidplu <david.didi.lu@icloud.com>
2026-09-05 17:33:16 -0700

    Add project configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,1 @@
+import { FlatCompat } from "@eslint/eslintrc"; const compat=new FlatCompat({baseDirectory:import.meta.dirname}); export default [{ignores:["node_modules/**",".next/**"]},...compat.extends("next/core-web-vitals","next/typescript")];
